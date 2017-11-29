bc39a7e0e60854e074893815f585c8d7e8d3a9f3
Angie Cunningham <angela.f.cunningham@gmail.com>
2017-11-29 02:08:43 +0000

    updated newsfeed style

--- index.js
+++ index.js
@@ -31,27 +31,40 @@ $(document).ready(function() {
             method: "GET",
             url: "https://newsapi.org/v2/top-headlines",
             data: { sources: document.getElementById('selection').value, apiKey: APIKEY},
-            success: function(data2){
-                
+            success: function(data2){ 
+                document.getElementById('newsfeed').innerHTML = "";
                 if (data2.status == "ok"){                        
                     console.log(data2);
+
                 
                     for (var i=0; i < data2.articles.length; i++){
+                        var maincontent = document.createElement("DIV");
+                        maincontent.setAttribute("class", "container");
+                        
+                        var row1 = document.createElement("DIV");
+                        row1.setAttribute('class', 'row');
+                        maincontent.appendChild(row1);
+                        
                         var displaynews = document.createElement("DIV");
-                        displaynews.setAttribute('id', 'newsbox');
-                        // displaynews.setAttribute('id', "HL" + i);
-                        var newstitle = document.createElement('H2')
-                        newstitle.innerHTML = data2.articles[i].title;
+                        displaynews.setAttribute('class', 'col-xs-4');
+                        row1.appendChild(displaynews);
+                        
+                        var newstitle = document.createElement('H2');
+                        var linktitle = document.createElement("A");
+                        linktitle.setAttribute('href', data2.articles[i].url);
+                        linktitle.innerHTML = data2.articles[i].title;
+                        newstitle.appendChild(linktitle);
                         displaynews.appendChild(newstitle);
+                        
                         var newsimage = document.createElement("IMG");
                         newsimage.setAttribute('src', data2.articles[i].urlToImage);
-                        // console.log(newsimage);
                         displaynews.appendChild(newsimage);
+                        
                         var descrip = document.createElement("P");
                         descrip.innerHTML = data2.articles[i].description;
                         displaynews.appendChild(descrip);
-                        document.getElementById('source').appendChild(displaynews);
-      
+
+                        document.getElementById('newsfeed').appendChild(displaynews);
                     }
                 }
             }
